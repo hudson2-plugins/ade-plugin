5d7773432e9ccff0fa2165bb452139e393d4a007
Jim Clark <slimslenderslacks@gmail.com>
2012-04-19 17:34:15 -0700

    iniial import of the ade plugin

---
+++ src/main/java/com/oracle/hudson/plugins/AdeViewLauncherDecorator.java
@@ -0,0 +1,221 @@
+package com.oracle.hudson.plugins;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.Launcher.ProcStarter;
+import hudson.Proc;
+import hudson.model.BuildListener;
+import hudson.model.TaskListener;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Run.RunnerAbortedException;
+import hudson.remoting.Channel;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildWrapperDescriptor;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class AdeViewLauncherDecorator extends BuildWrapper {
+	
+	private String viewName;
+	private String series;
+	
+	@DataBoundConstructor
+	public AdeViewLauncherDecorator(String view, String series) {
+		this.viewName = view;
+		this.series = series;
+	}
+	
+	public String getSeries() {
+		return this.series;
+	}
+	
+	public String getView() {
+		return this.viewName;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Launcher decorateLauncher(AbstractBuild build, Launcher launcher,
+			BuildListener listener) throws IOException, InterruptedException,
+			RunnerAbortedException {
+		
+		listener.getLogger().println("time to decorate");
+		
+		final Launcher outer = launcher;
+		final String[] prefix = new String[]{"ade","useview",viewName,"-exec"};
+		final BuildListener l = listener;
+		return new Launcher(outer) {
+            @Override
+            public Proc launch(ProcStarter starter) throws IOException {
+            	// don't prefix either createview or destroyview
+            	String[] args = starter.cmds().toArray(new String[]{});
+            	starter.envs(getEnvOverrides(starter.envs(),listener));
+            	if (args.length>1 && (args[1].equals("createview")||args[1].equals("destroyview"))) {
+            		l.getLogger().println("detected createview/destroyview");
+            		return outer.launch(starter);
+            	}
+            	// prefix everything else
+            	starter.cmds(prefix(starter.cmds().toArray(new String[]{})));
+                if (starter.masks() != null) {
+                    starter.masks(prefix(starter.masks()));
+                }
+                return outer.launch(starter);
+            }
+
+            @Override
+            public Channel launchChannel(String[] cmd, OutputStream out, FilePath workDir, Map<String, String> envVars) throws IOException, InterruptedException {
+            	if (cmd.length>1 && (cmd[1].equals("createview")||cmd[1].equals("destroyview"))) {
+            		l.getLogger().println("detected createview/destroyview in Channel");
+            		return outer.launchChannel(prefix(cmd),out,workDir,envVars);
+            	}
+                return outer.launchChannel(prefix(cmd),out,workDir,envVars);
+            }
+
+            @Override
+            public void kill(Map<String, String> modelEnvVars) throws IOException, InterruptedException {
+                outer.kill(modelEnvVars);
+            }
+
+            private String[] prefix(String[] args) {
+                //String[] newArgs = new String[args.length+prefix.length];
+                String[] newArgs = new String[prefix.length+1];
+                // copy prefix args into the front of the target array
+                System.arraycopy(prefix,0,newArgs,0,prefix.length);
+                // copy a single space-delimited String to tail of the new arg list
+                System.arraycopy(new String[]{spaceDelimitedStringArg(args)},0,newArgs,prefix.length,1);
+                return newArgs;
+            }
+            
+            private String spaceDelimitedStringArg(String[] args) {
+            	StringBuffer buffer = new StringBuffer();
+            	String prefix = "";
+            	for (String arg: args) {
+            		buffer.append(prefix+arg);
+            		prefix = " ";
+            	}
+            	return buffer.toString();
+            }
+
+            /*
+             * fyi:  masks are for "mask"ing out certain args that could contain sensitive information
+             * we don't use this in ADE
+             */
+            private boolean[] prefix(boolean[] args) {
+                boolean[] newArgs = new boolean[args.length+prefix.length];
+                System.arraycopy(args,0,newArgs,prefix.length,args.length);
+                return newArgs;
+            }
+        };
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Environment setUp(AbstractBuild build, Launcher launcher,
+			BuildListener listener) throws IOException, InterruptedException {
+		listener.getLogger().println("setup called:  ade createview");
+		ProcStarter procStarter = launcher.launch().cmds(new String[] {
+			"ade",
+			"createview",
+			"-latest",
+			"-series",
+			series,
+			viewName}).stdout(listener).stderr(listener.getLogger()).envs(getEnvOverrides());
+		Proc proc = launcher.launch(procStarter);
+		int exitCode = proc.join();
+		if (exitCode!=0) {
+			listener.getLogger().println("createview(success):  "+exitCode);
+			return new EnvironmentImpl(launcher);
+		} else {
+			listener.getLogger().println("createview:  "+exitCode);
+			return new EnvironmentImpl(launcher);
+		}
+	}
+	
+	private Map<String, String> getEnvOverrides(String[] keyValuePairs,TaskListener listener) {
+		Map<String,String> map = getEnvOverrides();
+        if (keyValuePairs!=null) {
+	        for( String keyValue: keyValuePairs ) {
+	        	String[] split = keyValue.split("=");
+	        	if (split.length<2) {
+	        		listener.getLogger().println(keyValue+" not in the correct format");
+	        	} else {
+	        		map.put(split[0],split[1]);
+	        	}
+	        }
+        }
+
+		return map;
+	}
+		
+	/**
+	 * ADE magic that we will need to expose as plugin-level config since all 3 of these settings depend on how
+	 * the slave is configured
+	 * 
+	 * Env overrides do not replace the base environment but augment.  In the case of the "PATH+XYZ" syntax, you can actually
+	 * prepend additional entries to PATH environment variables.  This is a special syntax that is specific to Hudson.
+	 * 
+	 * @return
+	 */
+	private Map<String, String> getEnvOverrides() {
+		Map<String,String> overrides = new HashMap<String,String>();
+		overrides.put("ADE_SITE","ade_slc");
+		overrides.put("ADE_DEFAULT_VIEW_STORAGE_LOC","/scratch/jamclark/view_storage");
+		// this is a special syntax that Hudson employs to allow us to prepend entries to the base PATH in 
+		// an OS-specific manner
+		overrides.put("PATH+INTG","/usr/local/packages/intg/bin");
+		overrides.put(UIPBuilder.seriesName,series);
+		return overrides;
+	}
+
+	class EnvironmentImpl extends Environment {
+		private Launcher launcher;
+		EnvironmentImpl(Launcher launcher) {
+			this.launcher = launcher;
+		}
+		@Override
+		public void buildEnvVars(Map<String, String> env) {
+			env.put(UIPBuilder.seriesName,series);
+		}
+		@SuppressWarnings("rawtypes")
+		@Override
+		public boolean tearDown(AbstractBuild build, BuildListener listener)
+				throws IOException, InterruptedException {
+			try {
+				listener.getLogger().println("tearing down:  ade destroyview");
+				ProcStarter procStarter = launcher.launch().cmds(new String[] {
+					"ade",
+					"destroyview",
+					viewName,
+					"-force"}).stdout(listener).stderr(listener.getLogger()).envs(getEnvOverrides());
+				Proc proc = launcher.launch(procStarter);
+				int exitCode = proc.join();
+				listener.getLogger().println("destroyview:  "+exitCode);
+			} catch (Exception e) {
+				listener.getLogger().println("Error destroying view:  "+e.getMessage());
+				return false;
+			}
+			return true;
+		}
+	}
+
+	@Extension
+	public static class DescriptorImpl extends BuildWrapperDescriptor {
+
+		@Override
+		public boolean isApplicable(AbstractProject<?, ?> arg0) {
+			return true;
+		}
+
+		@Override
+		public String getDisplayName() {
+			return "ADEBuildWrapper";
+		}
+	}
+}
---
+++ src/main/java/com/oracle/hudson/plugins/UIPBuilder.java
@@ -0,0 +1,126 @@
+package com.oracle.hudson.plugins;
+import hudson.AbortException;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.Launcher.ProcStarter;
+import hudson.Proc;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Sample {@link Builder}.
+ *
+ */
+public class UIPBuilder extends Builder {
+	private static final String newLabel = "New_Label";
+	public static final String seriesName = "Series_Name";
+    private final String task;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public UIPBuilder(String task) {
+        this.task = task;
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public String getTask() {
+        return task;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+    	try {
+        	EnvVars envVars = build.getEnvironment(listener);
+        	String label = null;
+        	if (!envVars.containsKey(newLabel)) {
+        		label = getNewLabel(envVars,listener);
+        		if (label==null) {
+        			throw new AbortException("builder has no series configured");
+        		}
+        		listener.getLogger().println("initialize new environment variable for "+newLabel+":  "+label);
+        		envVars.put(newLabel, label);
+        	} else {
+        		label = envVars.get(newLabel);
+        		listener.getLogger().println("use existing "+newLabel+":  "+label);
+        	}
+			ProcStarter procStarter = launcher.launch().cmds(
+				"integrate",
+				"-t",
+				task,
+				"--New_Label",
+				label
+				).stdout(listener).stderr(listener.getLogger());
+			Proc proc = launcher.launch(procStarter);
+			int exitCode = proc.join();
+			if (exitCode==0) {
+				return true;
+			}
+		} catch (IOException e) {
+			listener.fatalError(e.getMessage());
+		} catch (InterruptedException e) {
+			listener.fatalError(e.getMessage());
+		}
+        return false;
+    }
+    
+    public String getNewLabel(EnvVars envVars,BuildListener listener) {
+    	final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyMMdd.HHmm");
+    	if (envVars.containsKey(UIPBuilder.seriesName)) {
+    		String series = envVars.get(seriesName);
+    		return series+"_"+dateFormatter.format(new Date());
+    	} else {
+    		listener.error("no "+seriesName+" in environment");
+    		for (Map.Entry<String,String> entry: envVars.entrySet()) {
+    			listener.getLogger().println(entry.getKey()+":"+entry.getValue());
+    		}
+    		return "JRF_PT.POC1_GENERIC_"+dateFormatter.format(new Date());
+    	}
+    }
+
+    // overrided for better type safety.
+    // if your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link UIPBuilder}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>views/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // this marker indicates Hudson that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        @SuppressWarnings("rawtypes")
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Run a UIP task in an ADE view";
+        }
+    }
+}
+
