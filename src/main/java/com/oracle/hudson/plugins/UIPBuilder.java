da010caf6152541c30bfb49c21c8d5899e97b1c6
Ravi Kumar Raju V <ravikumar.vuddaraju@oracle.com>
2013-02-14 19:17:58 -0800

    Implement ER-16327117 - Update UIP Builder to provide a list of supported UIP Tasks, including Source Only Label - Invoke ADE with -noenv for UIP task except for build/postpublish/integrate

--- src/main/java/com/oracle/hudson/plugins/UIPBuilder.java
+++ src/main/java/com/oracle/hudson/plugins/UIPBuilder.java
@@ -15,7 +15,6 @@ import hudson.tasks.Builder;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
